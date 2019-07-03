898b56f855cc8b6beb68df204c3c9ccfd5f3dd4c
Colby Thomas <colby.thomas@parivedasolutions.com>
2019-07-03 11:09:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+NOTE! You will need to replace the path to the fillable PDF for this demo to work properly
---
+++ dotnet-syncfusion-playground/dotnet-syncfusion-playground/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_syncfusion_playground.Models;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
+using Syncfusion.Pdf.Parsing;
+using Microsoft.AspNetCore.Hosting;
+
+namespace dotnet_syncfusion_playground.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IHostingEnvironment _appEnvironment;
+
+        public HomeController(IHostingEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public FileStreamResult CreateDocument()
+        {
+            //Create a new PDF document
+            PdfDocument document = new PdfDocument();
+
+            //Add a page to the document
+            PdfPage page = document.Pages.Add();
+
+            //Create PDF graphics for the page
+            PdfGraphics graphics = page.Graphics;
+
+            //Set the standard font
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 12);
+
+            //Draw the text
+            graphics.DrawString("Michelle Thomas", font, PdfBrushes.Black, new PointF(50, 100));
+
+            //Saving the PDF to the MemoryStream
+            MemoryStream stream = new MemoryStream();
+
+            document.Save(stream);
+
+            //Set the position as '0'.
+            stream.Position = 0;
+
+            //Download the PDF document in the browser
+            FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/pdf");
+
+            fileStreamResult.FileDownloadName = "Sample.pdf";
+
+            return fileStreamResult;
+        }
+
+        public FileStreamResult TransposeData()
+        {
+            //Load the PDF document
+            FileStream docStream = new FileStream(_appEnvironment.WebRootPath + "\\fillable.pdf", FileMode.Open, FileAccess.Read);
+            PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+            //Loads the form
+            PdfLoadedForm form = loadedDocument.Form;
+            var foo = (form.Fields["Given Name Text Box"] as PdfLoadedTextBoxField).Bounds;
+            //Fills the textbox fields by using field name
+            (form.Fields["Given Name Text Box"] as PdfLoadedTextBoxField).Text = "John Cena";
+            //Save the PDF document to stream
+            MemoryStream stream = new MemoryStream();
+            loadedDocument.Save(stream);
+            //If the position is not set to '0' then the PDF will be empty.
+            stream.Position = 0;
+            //Close the document.
+            loadedDocument.Close(true);
+            //Defining the ContentType for pdf file.
+            string contentType = "application/pdf";
+            //Define the file name.
+            string fileName = "output.pdf";
+            //Creates a FileContentResult object by using the file contents, content type, and file name.
+            return File(stream, contentType, fileName);
+        }
+    }
+}
